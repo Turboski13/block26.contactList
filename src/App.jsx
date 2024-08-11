59188568308de9810b7480adfc774fdb91b64a1d
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-08-11 15:14:01 -0400

    tab 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import ContactList from ".components/ContactList";
+import ContactList from "./components/ContactList"
 import './App.css'
+
+const [contacts, setContacts] = useState(dummyContacts)
 
 export default function App() {
   return (
